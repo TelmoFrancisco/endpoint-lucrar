00c20e2fe80808ae6941e9ebbe20ca6ec85864fc
telmo <telmo_stb@hotmail.com>
2021-09-28 15:31:57 +0100

    abc

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
-app = Flask(__name__)
+from flask_frozen import Freezer
+from project import create_app
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# Call the application factory function to construct a Flask application
+# instance using the development configuration
+app = create_app()
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+# Create an instance of Freezer for generating the static files from
+# the Flask application routes ('/', '/breakfast', etc.)
+freezer = Freezer(app)
+
+
+if __name__ == '__main__':
+    # Run the development server that generates the static files
+    # using Frozen-Flask
+    freezer.run(debug=True)
